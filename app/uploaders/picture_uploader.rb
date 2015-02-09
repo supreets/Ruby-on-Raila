59de0c426294f7d4cb9b86033e43759cf73dc088
supreets <stonesupreet@gmail.com>
2015-02-09 14:28:01 +0000

    changed gemfile for heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,11 +6,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
    process resize_to_limit: [200,200]
+   
+   if Rails.env.production?
+        storage :fog
+    else
+        storage :file
+   end
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
 
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
